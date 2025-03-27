2d6ca87b0dab397c6c0032abbc3228bdda80dbd1
Amin Mahmudi <ma.mahmudi18@outlook.com>
2025-03-28 00:37:54 +0330

    fix: Some changes to making nvim better on windows

--- lua/plugins/tokyo-night.lua
+++ lua/plugins/tokyo-night.lua
@@ -2,7 +2,7 @@ return {
   "folke/tokyonight.nvim",
   config = function()
     vim.cmd("colorscheme tokyonight-storm")
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
   end
 }
