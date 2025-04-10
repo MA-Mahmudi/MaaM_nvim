de03bed649c2872e6a723b4e4ab38fde0597a0fa
Amin Mahmudi <ma.mahmudi18@outlook.com>
2025-04-10 12:32:59 +0330

    Remaps

--- lua/adara/remap.lua
+++ lua/adara/remap.lua
@@ -3,9 +3,13 @@ local keymap = vim.keymap
 keymap.set("n", "<leader>pv", "<cmd>Ex<CR>")
 keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true })
 
--- next greatest remap ever : asbjornHaland
-keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-keymap.set("n", "<leader>Y", [["+Y]])
+-- next greatest remap ever: asbjornHaland
+keymap.set({ "n", "v" }, "<leader>ys", [["+y]])
+keymap.set("n", "<leader>Ys", [["+Y]])
+
+-- next greatest remap ever: K1-end
+keymap.set("v", "<leader>y", [[mzy`z]])
+keymap.set("i", "<C-d>", "<C-o>dw")
 
 keymap.set("n", "<C-d>", "<C-d>zz")
 keymap.set("n", "<C-u>", "<C-u>zz")
