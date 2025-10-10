8e11938402beaa6e7168155491e6ecf5578fc41b
Amin Mahmudi <mahmudi.moam@gmail.com>
2025-10-10 18:54:20 +0330

    chore: Indents

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,12 +1,12 @@
 return {
-  "folke/tokyonight.nvim",
-  config = function()
-    vim.cmd("colorscheme tokyonight-moon")
-    vim.api.nvim_set_hl(0, "LineNr", { fg = "#ff7373" })
-    vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#805957" })
-    vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#805957" })
-    vim.api.nvim_set_hl(0, "signColumn", { bg = "none" })
-    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-  end
+    "folke/tokyonight.nvim",
+    config = function()
+        vim.cmd("colorscheme tokyonight-moon")
+        vim.api.nvim_set_hl(0, "LineNr", { fg = "#ff7373" })
+        vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#805957" })
+        vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#805957" })
+        vim.api.nvim_set_hl(0, "signColumn", { bg = "none" })
+        -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    end
 }
