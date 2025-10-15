a2dcad4cfdfc9007197abe7af447a4c4091640e8
ma-mahmudi <ma.mahmudi18@outlook.com>
2025-10-15 13:30:33 +0330

    feat: Added python to TS

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,25 +1,43 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require 'nvim-treesitter.configs'.setup {
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "go", "html", "css", "javascript", "vue", "typescript", "scss", "php", "blade" },
-        ignore_install = {},
-        modules = {},
-        auto_install = false,
-        highlight = {
-          enable = true,
-          disable = function(_, buf)
-            local max_filesize = 200 * 1024 -- 200 KB
-            local ok, stats = pcall(vim.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-              return true
-            end
-          end,
-        },
-        sync_install = false
-      }
-    end,
-  }
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require 'nvim-treesitter.configs'.setup {
+                ensure_installed = {
+                    "c",
+                    "lua",
+                    "vim",
+                    "vimdoc",
+                    "query",
+                    "markdown",
+                    "markdown_inline",
+                    "go",
+                    "html",
+                    "css",
+                    "javascript",
+                    "vue",
+                    "typescript",
+                    "scss",
+                    "php",
+                    "blade",
+                    "python"
+                },
+                ignore_install = { "javescript", "typescript" },
+                modules = {},
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                    disable = function(_, buf)
+                        local max_filesize = 100 * 1024 -- 100 KB
+                        local ok, stats = pcall(vim.fs_stat, vim.api.nvim_buf_get_name(buf))
+                        if ok and stats and stats.size > max_filesize then
+                            return true
+                        end
+                    end,
+                },
+                sync_install = false
+            }
+        end,
+    }
 }
