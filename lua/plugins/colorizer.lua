0a50f01f945635d34fa0aaa29679a3c261070083
ma-mahmudi <ma.mahmudi18@outlook.com>
2026-06-09 09:40:07 +0330

    style(colorizer): fix indentation

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,10 +1,10 @@
 return {
-  {
-    "norcalli/nvim-colorizer.lua",
-    config = function()
-      require('colorizer').setup({
-        '*', -- Apply to all file types
-      })
-    end
-  }
+    {
+        "norcalli/nvim-colorizer.lua",
+        config = function()
+            require('colorizer').setup({
+                '*', -- Apply to all file types
+            })
+        end
+    }
 }
